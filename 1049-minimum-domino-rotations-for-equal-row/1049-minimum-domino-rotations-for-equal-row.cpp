6503d874f6f75c68f6e6ece1c5d74b86c5926ab6
Kanishk Raj <114609861+KanishkRAJ29@users.noreply.github.com>
2025-05-03 14:47:56 +0530

    Time: 3 ms (67.46%), Space: 115.4 MB (76.51%) - LeetHub

---
+++ 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+int solveFor(int x, vector<int>& T, vector<int>& B) {
+  int flipT = 0, flipB = 0;
+  for (int i = 0; i < T.size(); i++) {
+    if (T[i] != x && B[i] != x) return -1;
+    if (T[i] != x) ++flipT;   // must flip from B→T
+    if (B[i] != x) ++flipB;   // must flip from T→B
+  }
+  return min(flipT, flipB);
+}
+    int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
+         int a = solveFor(tops[0], tops, bottoms);
+  int b = solveFor(bottoms[0], tops, bottoms);
+  if (a == -1 && b == -1) return -1;
+  if (a == -1) return b;
+  if (b == -1) return a;
+  return min(a, b);
+    }
+};
