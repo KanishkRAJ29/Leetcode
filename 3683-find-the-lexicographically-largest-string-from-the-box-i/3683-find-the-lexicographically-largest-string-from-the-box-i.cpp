141030e2335ac2c3a5afed2dfeba6ce2b35d1028
Kanishk Raj <114609861+KanishkRAJ29@users.noreply.github.com>
2025-06-10 07:35:05 +0530

    Time: 15 ms (79.2%), Space: 36.6 MB (77.6%) - LeetHub

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.cpp
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    string lastSubstring(string s) {
-        int i = 0, j = 1, n = s.size();
-        while (j < n) {
-            int k = 0;
-            while (j + k < n && s[i + k] == s[j + k]) {
-                k++;
-            }
-            if (j + k < n && s[i + k] < s[j + k]) {
-                int t = i;
-                i = j;
-                j = max(j + 1, t + k + 1);
-            } else {
-                j = j + k + 1;
+    string answerString(string s, int k) {
+       if(k==1)return s;
+       char largeChar='a';
+       int ind=0;
+       for(int i=0;i<s.size();i++){
+        if(largeChar>=s[i])continue;
+        largeChar=s[i];
+        ind=i;
+       }
+       string result="";
+       for (int i = 0; i < s.size(); i++) {
+            if (s[i] == largeChar) {
+                 int s_possible_beforeind=i+1;
+                 string candidate="";
+                if(s_possible_beforeind>=k){
+                    candidate= s.substr(i,s.size()-i);
+                }else{
+                    int left=k-s_possible_beforeind;
+                    candidate= s.substr(i,s.size()-left-i);
+                }
+                if (candidate > result) result = candidate;
             }
         }
-        return s.substr(i, n - i);
-    }
-
-    string answerString(string word, int numFriends) {
-        if (numFriends == 1) {
-            return word;
-        }
-        string last = lastSubstring(word);
-        int n = word.size(), m = last.size();
-        return last.substr(0, min(m, n - numFriends + 1));
+      
+    return result;
     }
 };
