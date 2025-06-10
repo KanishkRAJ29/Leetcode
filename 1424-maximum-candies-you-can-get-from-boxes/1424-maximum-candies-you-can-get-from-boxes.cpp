90d594dc016aed14cafc8d05017a67a071bc3d82
Kanishk Raj <114609861+KanishkRAJ29@users.noreply.github.com>
2025-06-10 08:16:40 +0530

    Time: 4 ms (52.21%), Space: 44.6 MB (38%) - LeetHub

--- 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.cpp
+++ 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.cpp
@@ -1,47 +1,44 @@
 class Solution {
 public:
-    int maxCandies(vector<int>& status, vector<int>& candies,
+    int maxCandies(vector<int>& status,
+                   vector<int>& candies,
                    vector<vector<int>>& keys,
                    vector<vector<int>>& containedBoxes,
                    vector<int>& initialBoxes) {
-        int n = status.size();
-        vector<bool> can_open(n), has_box(n), used(n);
-        for (int i = 0; i < n; ++i) {
-            can_open[i] = (status[i] == 1);
-        }
-
-        queue<int> q;
-        int ans = 0;
-        for (int box : initialBoxes) {
-            has_box[box] = true;
-            if (can_open[box]) {
-                q.push(box);
-                used[box] = true;
-                ans += candies[box];
-            }
-        }
-
-        while (!q.empty()) {
-            int big_box = q.front();
-            q.pop();
-            for (int key : keys[big_box]) {
-                can_open[key] = true;
-                if (!used[key] && has_box[key]) {
-                    q.push(key);
-                    used[key] = true;
-                    ans += candies[key];
+        int total = 0;
+        // work on our own copy so we can push_back without disturbing caller
+        vector<int> boxes = initialBoxes;
+        
+        // status[i]: 0 = closed, 1 = open & unprocessed, 2 = already processed
+        bool progress = true;
+        while (progress) {
+            progress = false;
+            int sz = boxes.size();
+            // try to process every box we currently “have”
+            for (int i = 0; i < sz; ++i) {
+                int b = boxes[i];
+                // only open AND not yet processed
+                if (status[b] != 1) 
+                    continue;
+                
+                // collect its candies
+                total += candies[b];
+                status[b] = 2;           // mark “processed”
+                progress = true;         // we made progress this round
+                
+                // use any new keys
+                for (int k : keys[b]) {
+                    if (status[k] == 0) {
+                        status[k] = 1;   // open it
+                    }
                 }
-            }
-            for (int box : containedBoxes[big_box]) {
-                has_box[box] = true;
-                if (!used[box] && can_open[box]) {
-                    q.push(box);
-                    used[box] = true;
-                    ans += candies[box];
+                // pick up any boxes inside
+                for (int nb : containedBoxes[b]) {
+                    boxes.push_back(nb);
                 }
             }
         }
-
-        return ans;
+        
+        return total;
     }
 };
