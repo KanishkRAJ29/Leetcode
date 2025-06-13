06201a9d7ee95ad72f60d9d543069d896ace9408
Kanishk Raj <114609861+KanishkRAJ29@users.noreply.github.com>
2025-06-13 12:28:01 +0530

    Time: 487 ms (13.53%), Space: 396.5 MB (5.95%) - LeetHub

--- 3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii/3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii.cpp
+++ 3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii/3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii.cpp
@@ -1,40 +1,50 @@
 class Solution {
 public:
-    int dfs(int node, int parent, int depth,
-            const vector<vector<int>>& children, vector<int>& color) {
-        int res = 1 - depth % 2;
-        color[node] = depth % 2;
-        for (int child : children[node]) {
-            if (child == parent) {
-                continue;
+        unordered_map<int,vector<int>>giveAdj(vector<vector<int>>&edges){
+            unordered_map<int,vector<int>>adj;
+            for(int i=0;i<edges.size();i++){
+                int u=edges[i][0];
+                int v=edges[i][1];
+                adj[u].push_back(v);
+                adj[v].push_back(u);
             }
-            res += dfs(child, node, depth + 1, children, color);
+            return adj;
         }
-        return res;
-    }
+    void dfs(int curr,vector<int>&marking,unordered_map<int,vector<int>>&adj,int& zeros,int& ones,int parent){
+        if(marking[curr]==0)zeros++;
+        else ones++;
 
-    vector<int> build(const vector<vector<int>>& edges, vector<int>& color) {
-        int n = edges.size() + 1;
-        vector<vector<int>> children(n);
-        for (const auto& edge : edges) {
-            children[edge[0]].push_back(edge[1]);
-            children[edge[1]].push_back(edge[0]);
+        for(auto &ngbr:adj[curr]){
+            if(ngbr==parent)continue;
+            marking[ngbr]=(marking[curr]==0) ? 1 : 0;
+            dfs(ngbr,marking,adj,zeros,ones,curr);
         }
-        int res = dfs(0, -1, 0, children, color);
-        return {res, n - res};
     }
+    vector<int> maxTargetNodes(vector<vector<int>>& edges1, vector<vector<int>>& edges2) {
+        int n=edges1.size()+1;
+        int m=edges2.size()+1;
+        unordered_map<int,vector<int>>adj1=giveAdj(edges1);
+        unordered_map<int,vector<int>>adj2=giveAdj(edges2);
+        int zmA=0;//zero marked in A
+        int omA=0;//one marked in A
+        vector<int>markingA(n,-1);
+        markingA[0]=0;
+        dfs(0,markingA,adj1,zmA,omA,-1);
 
-    vector<int> maxTargetNodes(vector<vector<int>>& edges1,
-                               vector<vector<int>>& edges2) {
-        int n = edges1.size() + 1, m = edges2.size() + 1;
-        vector<int> color1(n);
-        vector<int> color2(m);
-        vector<int> count1 = build(edges1, color1);
-        vector<int> count2 = build(edges2, color2);
-        vector<int> res(edges1.size() + 1);
-        for (int i = 0; i < n; i++) {
-            res[i] = count1[color1[i]] + max(count2[0], count2[1]);
+        int zmB=0;//zero marked in B
+        int omB=0;//one marked in B
+        vector<int>markingB(m,-1);
+        markingB[0]=0;
+        dfs(0,markingB,adj2,zmB,omB,-1);
+        int maxi=max(omB,zmB);
+        vector<int>result(n,0);
+        for(int i=0;i<n;i++){
+            if(markingA[i]==0){
+                result[i]=zmA+maxi;
+            }else{
+                result[i]=omA+maxi;
+            }
         }
-        return res;
+        return result;
     }
 };
