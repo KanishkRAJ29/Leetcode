13c57a96a15753584daf50d9c995a63de6998171
Kanishk Raj <114609861+KanishkRAJ29@users.noreply.github.com>
2025-06-14 17:31:24 +0530

    Time: 35 ms (64.56%), Space: 171.7 MB (90.1%) - LeetHub

--- 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -1,20 +1,46 @@
 class Solution {
 public:
     int longestPalindrome(vector<string>& words) {
-        unordered_map<string, int> mpp;
-        for (string w : words) mpp[w]++;
-
-        int count = 0, alreadyPalindrome = 0;
-        for (auto& [w, freq] : mpp) {
-            string s = w;
-            reverse(s.begin(), s.end());
-            if (w == s) {
-                count += (freq / 2) * 4;
-                if (freq % 2) alreadyPalindrome = 1;
-            } else if (w < s && mpp.count(s)) {
-                count += min(freq, mpp[s]) * 4;
-            }
+        unordered_map<string,int>freq;
+        for(int i=0;i<words.size();i++){
+            string s=words[i];
+            freq[s]++;
         }
-        return count + alreadyPalindrome * 2;
+        int odd=0;
+        int ans=0;
+        for(auto&[key,value]: freq){
+            string s=key;
+            int f1=value;
+            if(f1==0)continue;
+            if(s[0]==s[1]){
+               if(f1%2==1){
+                if(f1>1){
+                    ans+=2*(f1-1);
+                }
+                if(odd==0){
+                    odd=1;
+                    ans+=2;
+                    continue;
+                }
+                continue;
+            }else{
+                ans+=2*f1;
+            } 
+            }else{
+                string r = "";
+                r += s[1];
+                r += s[0];
+                if(freq.count(r)){
+                    ans+=min(freq[r],freq[s]);
+                    ans+=min(freq[r],freq[s]);
+                    ans+=min(freq[r],freq[s]);
+                    ans+=min(freq[r],freq[s]);
+                    freq[r]=0;
+                    continue;
+                }
+            }
+            
+        }
+        return ans;
     }
 };
