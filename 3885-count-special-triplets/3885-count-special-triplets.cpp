564143b89f0b9ff4c3e6c5dbd2fa96430b9e2470
Kanishk Raj <114609861+KanishkRAJ29@users.noreply.github.com>
2025-06-15 18:23:21 +0530

    Time: 598 ms (63.64%), Space: 362.7 MB (54.55%) - LeetHub

--- 3885-count-special-triplets/3885-count-special-triplets.cpp
+++ 3885-count-special-triplets/3885-count-special-triplets.cpp
@@ -1,23 +1,50 @@
-const int MOD = 1e9 + 7;
 class Solution {
 public:
+    int MOD;
+
+    int findCount(const vector<int>& indices, int midind) {
+        int small = upper_bound(indices.begin(), indices.end(), midind) - indices.begin();
+        int large = indices.size() - small;
+        return (1LL * small * large) % MOD;
+    }
+
     int specialTriplets(vector<int>& nums) {
-        int n = nums.size();
-        long long result = 0;
-        unordered_map<int, int> r, l;
-        for (int val : nums) {
-            r[val]++;
+        unordered_map<int, vector<int>> mp;
+        for (int i = 0; i < nums.size(); i++) {
+            mp[nums[i]].push_back(i);
         }
 
-        for (int j = 0; j < n; ++j) {
-            int mid = nums[j];
-            r[mid]--; 
-            int left = l[2 * mid];
-            int right = r[2 * mid];
-            result = (result + 1LL * left * right) % MOD;
-            l[mid]++;
+        for (auto& [_, vec] : mp) {
+            sort(vec.begin(), vec.end()); // Required for binary search
         }
 
-        return result;
+        MOD = 1e9 + 7;
+        long long ans = 0;
+
+        sort(nums.begin(), nums.end(), greater<int>());
+        nums.erase(unique(nums.begin(), nums.end()), nums.end());
+
+        for (int x : nums) {
+            if (x % 2 == 1) continue;
+            int mid = x / 2;
+            if (!mp.count(mid)) continue;
+
+            const auto& midIndices = mp[mid];
+            const auto& xIndices = mp[x];
+
+            if (x == mid) {
+                int n = midIndices.size();
+                if (n >= 3) {
+                    ans = (ans + (1LL * n * (n - 1) % MOD * (n - 2) % MOD * 166666668LL) % MOD) % MOD;
+                }
+                continue;
+            }
+
+            for (int s : midIndices) {
+                ans = (ans + findCount(xIndices, s)) % MOD;
+            }
+        }
+
+        return (int)ans;
     }
 };
