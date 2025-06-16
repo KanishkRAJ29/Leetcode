526dae2cf507eeecdba9de01665b13c2472d9682
Kanishk Raj <114609861+KanishkRAJ29@users.noreply.github.com>
2025-06-16 15:55:12 +0530

    Time: 143 ms (8.67%), Space: 224.3 MB (57.32%) - LeetHub

--- 3647-zero-array-transformation-iii/3647-zero-array-transformation-iii.cpp
+++ 3647-zero-array-transformation-iii/3647-zero-array-transformation-iii.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     int maxRemoval(vector<int>& nums, vector<vector<int>>& queries) {
-        sort(queries.begin(), queries.end(),
-             [](const vector<int>& a, const vector<int>& b) {
-                 return a[0] < b[0];
-             });
-
-        priority_queue<int> heap;
-        vector<int> deltaArray(nums.size() + 1, 0);
-        int operations = 0;
-        for (int i = 0, j = 0; i < nums.size(); ++i) {
-            operations += deltaArray[i];
-            while (j < queries.size() && queries[j][0] == i) {
-                heap.push(queries[j][1]);
-                ++j;
+        int n=nums.size();
+        int q=queries.size();
+        sort(queries.begin(),queries.end());
+        priority_queue<int>ending;
+        priority_queue<int,vector<int>,greater<int>>past;
+        int j=0;
+        int usecount=0;
+        for(int i=0;i<n;i++){
+            while(j<q&&queries[j][0]==i){
+                ending.push(queries[j][1]);
+                j++;
             }
-            while (operations < nums[i] && !heap.empty() && heap.top() >= i) {
-                operations += 1;
-                deltaArray[heap.top() + 1] -= 1;
-                heap.pop();
+            nums[i]-=past.size();
+            while(nums[i]>0&&!ending.empty()&&ending.top()>=i){
+                int end=ending.top();
+                ending.pop();
+                usecount++;
+                nums[i]--;
+                past.push(end);
             }
-            if (operations < nums[i]) {
-                return -1;
+            if(nums[i]>0)return -1;
+            while(!past.empty()&&past.top()<=i){
+                past.pop();
             }
         }
-        return heap.size();
+        return q-usecount;
     }
 };
