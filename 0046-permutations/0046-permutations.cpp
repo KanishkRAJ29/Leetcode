0c5838cec26fff96de5286087c7df6623c103b77
Kanishk Raj <114609861+KanishkRAJ29@users.noreply.github.com>
2025-06-30 18:22:07 +0530

    Time: 0 ms (100%), Space: 10.9 MB (32.03%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -3,7 +3,8 @@ public:
     vector<vector<int>>ans;
 
     void solve(vector<int>&temp,vector<int>&nums){
-        if(temp.size()==nums.size())ans.push_back(temp);
+        if(temp.size()==nums.size()){ans.push_back(temp);
+        return;}
         for(int i=0;i<nums.size();i++){
             if(find(temp.begin(),temp.end(),nums[i])==temp.end()){
                 temp.push_back(nums[i]);
