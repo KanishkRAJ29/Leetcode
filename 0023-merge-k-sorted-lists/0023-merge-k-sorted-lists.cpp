b9302a92dd7f4ef9c1669250ab1d424c1004bc74
Kanishk Raj <114609861+KanishkRAJ29@users.noreply.github.com>
2025-07-04 15:27:51 +0530

    Time: 0 ms (100%), Space: 18.2 MB (90.8%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -18,18 +18,17 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-/*
- class compare{
-  public:
-  bool operator()(ListNode*a ,ListNode*b){
-      return a->val>b->val;
-  }  
-};
+
+
 
 class Solution {
 public:
+    
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<ListNode*, vector<ListNode*>, compare> pq;
+        auto compare=[](ListNode*a,ListNode*b){
+        return a->val>b->val;
+    };
+        priority_queue<ListNode*, vector<ListNode*>,decltype(compare) > pq(compare);
 
         for(int i=0; i<lists.size(); i++){
             ListNode* listHead= lists[i];
@@ -62,7 +61,7 @@ public:
          }
         return head;
     }
-};*/
+};/*
 class Solution {
 public:
     ListNode* merge(ListNode* l1,ListNode* l2){
@@ -89,4 +88,4 @@ public:
         if(lists.size()==0)return NULL;
       return  partition(0,lists.size()-1,lists);
     }
-};
+};*/
