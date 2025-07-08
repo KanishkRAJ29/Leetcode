3121a17b9245681c223127e2cb7788f5f06f1434
Kanishk Raj <114609861+KanishkRAJ29@users.noreply.github.com>
2025-07-08 08:54:02 +0530

    Time: 4 ms (59.44%), Space: 10.2 MB (39.58%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.cpp
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -1,44 +1,45 @@
-/*class Solution {
+class Solution {
+private:
+    int BASE = 1000000;
 public:
-    bool check(string b,string a){
-        char fa=a[0];
-        int len=a.length();
-        string s="";
-        for(int i=0;i<b.length();i++){
-            if(b[i]==fa){
-                s=b.substr(i,len);
-                if(s==a)return true;
-            }
+    int repeatedStringMatch(string A, string B) {
+        if(A == B) return 1;
+        int count = 1;
+        string source = A;
+        while(source.size() < B.size()){
+            count++;
+            source+=A;
         }
-        return false;
-    }
-    int repeatedStringMatch(string a, string b) {
-        bool c=check(b,a);
-        string s="";
-        int n=(b.length()/a.length())+a.length();
-        for(int i=1;i<n;i++){
-            s+=a;
-            if(check(s,b))return i;
-        }
+        if(source == B) return count;
+        if(Rabin_Karp(source,B) != -1) return count;
+        if(Rabin_Karp(source+A,B) != -1) return count+1;
         return -1;
     }
-};*/class Solution {
-public:
-    int repeatedStringMatch(string a, string b) {
-        string repeated = a;
-        int count = 1;
-
-        // Keep appending 'a' until the length of repeated string >= length of b
-        while (repeated.length() < b.length()) {
-            repeated += a;
-            count++;
+    int Rabin_Karp(string source, string target){
+        if(source == "" or target == "") return -1;
+        int m = target.size();
+        int power = 1;
+        for(int i = 0;i<m;i++){
+            power = (power*31)%BASE;
         }
-
-        // Check if b is a substring now or after one more repeat
-        if (repeated.find(b) != string::npos) return count;
-        repeated += a;
-        if (repeated.find(b) != string::npos) return count + 1;
-
+        int targetCode = 0;
+        for(int i = 0;i<m;i++){
+            targetCode = (targetCode*31+target[i])%BASE;
+        }
+        int hashCode = 0;
+        for(int i = 0;i<source.size();i++){
+            hashCode = (hashCode*31 + source[i])%BASE;
+            if(i<m-1) continue;
+            if(i>=m){
+                hashCode = (hashCode-source[i-m]*power)%BASE;
+            }
+            if(hashCode<0)
+                hashCode+=BASE;
+            if(hashCode == targetCode){
+                if(source.substr(i-m+1,m) == target)
+                    return i-m+1;
+            }
+        }
         return -1;
     }
 };
