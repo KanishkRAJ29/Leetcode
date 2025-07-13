648a46ab8e159e75650f2bbfae5c0bc3490f5190
Kanishk Raj <114609861+KanishkRAJ29@users.noreply.github.com>
2025-07-13 07:52:02 +0530

    Time: 571 ms (5%), Space: 80.3 MB (60.41%) - LeetHub

--- 2497-maximum-matching-of-players-with-trainers/2497-maximum-matching-of-players-with-trainers.cpp
+++ 2497-maximum-matching-of-players-with-trainers/2497-maximum-matching-of-players-with-trainers.cpp
@@ -1,19 +1,34 @@
 class Solution {
 public:
-    int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
-        sort(players.begin(), players.end());
-        sort(trainers.begin(), trainers.end());
-        
-        int i = 0, j = 0, count = 0;
-        while (i < players.size() && j < trainers.size()) {
-            if (players[i] <= trainers[j]) {
-                count++;
-                i++;
-                j++;
-            } else {
-                j++;
-            }
+    int matchPlayersAndTrainers(vector<int>& p, vector<int>& t) {
+    if(p.size()==0||t.size()==0)return 0;
+    if(p.size()==1||t.size()==1){
+        if(p[0]<=t[0])return 1;
+        else return 0;}
+     int ans=0;
+     sort(p.begin(),p.end());
+     sort(t.begin(),t.end());
+    if(p==t)return p.size();
+     for(int i=0;i<p.size();i++){
+        int x=p[i];
+        int y=-1;
+        int j;
+        int low=0;
+        int high=t.size()-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(t[mid]>=x){
+                high=mid-1;
+                y=t[mid];
+                j=mid;
+            }else low=mid+1;
         }
-        return count;
+        if(y>=x){
+            ans++;
+            t.erase(t.begin()+j);
+        }
+        
+     }   
+     return ans;
     }
 };
