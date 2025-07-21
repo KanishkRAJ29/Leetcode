b078d280ed1cac060d04f449dadad3aab6fd82ef
Kanishk Raj <114609861+KanishkRAJ29@users.noreply.github.com>
2025-07-21 12:27:39 +0530

    Time: 0 ms (100%), Space: 10 MB (97.38%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int solve(vector<int>& nums, int memo[101][2], int i, int last) {
+        int n = nums.size();
+        if (i >= n) return 0; // Base case: no houses left
+        
+        if (memo[i][last] != -1) return memo[i][last]; // Check memoization
+        
+        int taken = 0, notTaken = 0;
+
+        if (i == 0) {
+            // At the first house, we can rob or skip
+            taken = nums[i] + solve(nums, memo, i + 2, 0);
+            notTaken = solve(nums, memo, i + 1, 1);
+        } 
+        else if (i == n - 1) {
+            // At the last house, only rob if the first house is not robbed
+            if (last == 1) taken = nums[i] + solve(nums, memo, i + 2, last);
+            notTaken = solve(nums, memo, i + 1, last);
+        } 
+        else {
+            // For other houses, consider both options
+            taken = nums[i] + solve(nums, memo, i + 2, last);
+            notTaken = solve(nums, memo, i + 1, last);
+        }
+
+        return memo[i][last] = max(taken, notTaken);
+    }
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) return nums[0]; // Edge case: only one house
+        
+        // Create memoization table and initialize to -1
+        int memo[101][2];
+        memset(memo, -1, sizeof(memo));
+        
+        // Solve the problem starting from the first house and allowing circular constraint
+        return solve(nums, memo, 0, 1);
+    }
+};
