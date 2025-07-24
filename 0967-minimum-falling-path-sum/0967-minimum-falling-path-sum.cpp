47479daff2de5c8b816e0cae77edb43dafc71636
Kanishk Raj <114609861+KanishkRAJ29@users.noreply.github.com>
2025-07-24 12:28:33 +0530

    Time: 3 ms (32.59%), Space: 15 MB (39.06%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -1,4 +1,26 @@
-int dp[101][101];
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& m) {
+        vector<vector<int>>dp(m.size(),vector<int>(m[0].size(),INT_MAX));
+        for(int i=0;i<m[0].size();i++){
+            dp[m.size()-1][i]=m[m.size()-1][i];
+        }
+        for(int x=m.size()-2;x>=0;x--){
+            for(int y=m[0].size()-1;y>=0;y--){
+                int left=INT_MAX;
+                if(y-1>=0)left=dp[x+1][y-1];
+                int right=INT_MAX;
+                if(y+1<m[0].size())right=dp[x+1][y+1];
+                dp[x][y]=m[x][y]+min({dp[x+1][y],left,right});
+            }
+        }
+        int ans=INT_MAX;
+        for(int i=0;i<m[0].size();i++){
+            ans=min(ans,dp[0][i]);
+        }
+        return ans;
+    }
+};/*int dp[101][101];
 int n;
 
 int find(int x, int y, vector<vector<int>>& g)
@@ -30,4 +52,4 @@ public:
         
         return ans;
     }
-};
+};*/
