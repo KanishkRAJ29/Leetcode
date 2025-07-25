e1278033ac1bdceb72f24173b26af3a42465dbc4
Kanishk Raj <114609861+KanishkRAJ29@users.noreply.github.com>
2025-07-25 08:15:43 +0530

    Time: 15 ms (53.66%), Space: 24.4 MB (35.66%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -7,19 +7,22 @@ public:
         if (abs(target) > totalSum) return 0;
 
         // Initialize dp array with INT_MIN
-        vector<vector<int>> dp(nums.size(), vector<int>(2 * totalSum + 1, 0));
-        dp[0][nums[0]+totalSum]=1;
-        dp[0][-nums[0]+totalSum]+=1;
+        vector<int>prev(2 * totalSum + 1, 0);
+        prev[nums[0]+totalSum]=1;
+        prev[-nums[0]+totalSum]+=1;
         for(int i=1;i<nums.size();i++){
+        vector<int>curr(2 * totalSum + 1, 0);
+
             for(int sum=-totalSum;sum<=totalSum;sum++){
                 
-                if(dp[i-1][sum+totalSum]>0){
-                    dp[i][sum + nums[i]+totalSum] += dp[i - 1][sum+totalSum];
-                    dp[i][sum - nums[i]+totalSum] += dp[i - 1][sum+totalSum];
+                if(prev[sum+totalSum]>0){
+                    curr[sum + nums[i]+totalSum] += prev[sum+totalSum];
+                    curr[sum - nums[i]+totalSum] += prev[sum+totalSum];
                 }
             }
+            prev=curr;
         }
-        return dp[nums.size()-1][target + totalSum];
+        return prev[target + totalSum];
 
     }
 };
