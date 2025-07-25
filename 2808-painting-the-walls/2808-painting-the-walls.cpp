5fc8d6e10429dd9f184625b85ff1994ad8389eed
Kanishk Raj <114609861+KanishkRAJ29@users.noreply.github.com>
2025-07-25 08:59:48 +0530

    Time: 48 ms (75.78%), Space: 121.9 MB (46.14%) - LeetHub

--- 2808-painting-the-walls/2808-painting-the-walls.cpp
+++ 2808-painting-the-walls/2808-painting-the-walls.cpp
@@ -1,5 +1,22 @@
 class Solution {
 public:
+    int paintWalls(vector<int>& cost, vector<int>& time) {
+        int n = cost.size();
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        for (int i = 1; i <= n; i++) {
+            dp[n][i] = 1e9;
+        }
+        for(int i=n-1;i>=0;i--){
+            for(int rem=1;rem<=n;rem++){
+                int paint=cost[i]+dp[i+1][max(0,rem-1-time[i])];
+                int free=dp[i+1][rem];
+                dp[i][rem]=min(free,paint);
+            }
+        }
+        return dp[0][n];
+    }
+};/*class Solution {
+public:
     int solve(int i, int wallsLeft, vector<int>& cost, vector<int>& time, vector<vector<int>>& dp) {
         if (wallsLeft <= 0) return 0;
         if (i == cost.size()) return 1e9;  // Impossible
@@ -21,3 +38,4 @@ public:
         return solve(0, n, cost, time, dp);
     }
 };
+*/
