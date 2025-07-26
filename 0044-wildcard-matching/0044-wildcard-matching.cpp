abc92ab75a695d3648c698a9d6ebdbadf03ce40e
Kanishk Raj <114609861+KanishkRAJ29@users.noreply.github.com>
2025-07-26 12:50:55 +0530

    Time: 35 ms (43.23%), Space: 12 MB (86.12%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -3,6 +3,34 @@ public:
     bool isMatch(string s, string p) {
         int n=s.size();
         int m=p.size();
+        vector<bool>prev(m+1,false);
+        prev[0]=true;
+        for (int j = 1; j <= m; j++) {
+            if (p[j - 1] == '*')
+                prev[j] = prev[j - 1];
+        }
+        for(int i=1;i<n+1;i++){
+        vector<bool>curr(m+1,false);
+
+            for(int j=1;j<m+1;j++){
+                if (s[i-1] == p[j-1] || p[j-1] == '?') {  
+                curr[j] = prev[j-1];
+                }
+                if (p[j-1] == '*') {
+                curr[j] = prev[j] || curr[j-1];
+            // '*' matches 1+ chars OR '*' matches 0 chars
+                }
+            }
+            prev=curr;
+        }
+        return prev[m];
+    }
+};
+/*class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int n=s.size();
+        int m=p.size();
         vector<vector<bool>>dp(n+1,vector<bool>(m+1,false));
         dp[0][0]=true;
         for (int j = 1; j <= m; j++) {
@@ -22,7 +50,7 @@ public:
         }
         return dp[n][m];
     }
-};
+};*/
 /*class Solution {
 public:
     int dp[2001][2001];
