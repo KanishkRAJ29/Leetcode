826ad8dd664fddbd50003b8f472dca16c028465c
Kanishk Raj <114609861+KanishkRAJ29@users.noreply.github.com>
2025-07-26 12:16:29 +0530

    Time: 7 ms (59.4%), Space: 13.3 MB (27.8%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,5 +1,32 @@
 class Solution {
 public:
+    int minDistance(string w1, string w2) {
+        int n=w1.size();
+        int m=w2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=0;i<m+1;i++){
+            dp[0][i]=i;
+        }
+        for(int i=0;i<n+1;i++){
+            dp[i][0]=i;
+        }
+        int i=1,j=1;
+        for( i=1;i<n+1;i++){
+            for( j=1;j<m+1;j++){
+                if(w1[i-1]==w2[j-1])dp[i][j]=dp[i-1][j-1];
+                else{
+                    int in=dp[i][j-1];
+                    int del=dp[i-1][j];
+                    int rep=dp[i-1][j-1];
+                    dp[i][j]=1+min({in,del,rep});
+                }
+            }
+        }
+        return dp[n][m];
+        
+    }
+};/*class Solution {
+public:
     int dp[501][501];
     int m,n;
     int solve(string s1, string s2,int i,int j){
@@ -23,4 +50,4 @@ public:
         memset(dp,-1,sizeof(dp));
         return solve(word1,word2,0,0);
     }
-};
+};*/
