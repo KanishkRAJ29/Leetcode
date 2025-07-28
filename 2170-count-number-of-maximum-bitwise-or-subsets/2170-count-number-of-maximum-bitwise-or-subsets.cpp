bf77fe24ba5fa25d412e62c8764ec0d6abb375d4
Kanishk Raj <114609861+KanishkRAJ29@users.noreply.github.com>
2025-07-28 07:54:04 +0530

    Time: 235 ms (14.25%), Space: 313.6 MB (8.7%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,5 +1,33 @@
 class Solution {
 public:
+    int countMaxOrSubsets(vector<int>& nums) {
+        int n = nums.size();
+        int maxOR = 0;
+
+        for (int x : nums) maxOR |= x;
+
+        // OR values will range from 0 to maxOR (max 2^17 ~ 131072)
+        vector<vector<int>> dp(n + 1, vector<int>(maxOR + 1, 0));
+        dp[0][0] = 1; // 0 elements, OR = 0
+
+        for (int i = 0; i < n; i++) {
+            for (int or_val = 0; or_val <= maxOR; or_val++) {
+                if (dp[i][or_val] == 0) continue;
+
+                // Skip current number
+                dp[i + 1][or_val] += dp[i][or_val];
+
+                // Take current number
+                int new_or = or_val | nums[i];
+                dp[i + 1][new_or] += dp[i][or_val];
+            }
+        }
+
+        return dp[n][maxOR];
+    }
+};
+/*class Solution {
+public:
     int solve(int i,int val,int maxval,vector<int>&nums,vector<vector<int>>&dp){
         if(val==maxval)return 1<<(nums.size()-i);
         if(i>=nums.size())return 0;
@@ -16,4 +44,4 @@ public:
         vector<vector<int>>dp(nums.size()+1,vector<int>(maxval+1,-1));
         return solve(0,0,maxval,nums,dp);
     }
-};
+};*/
