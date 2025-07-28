26fd4e37728362ed083b0da8d0392a1e2d2d7a51
Kanishk Raj <114609861+KanishkRAJ29@users.noreply.github.com>
2025-07-28 07:09:16 +0530

    Time: 152 ms (5.04%), Space: 176.4 MB (6.36%) - LeetHub

---
+++ 3766-maximum-median-sum-of-subsequences-of-size-3/3766-maximum-median-sum-of-subsequences-of-size-3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long maximumMedianSum(vector<int>& nums) {
+        if(nums.size()==0)return 0;
+        int n=nums.size();
+        sort(nums.rbegin(),nums.rend());
+           long long ans=0;
+        int j=n-1;
+        for(int i=1;i<n;i+=2){
+            if(nums[i]==0)continue;
+            nums[j]=0;
+            j--;
+           ans+=nums[i];
+        }
+        return ans;
+        
+    }
+};
