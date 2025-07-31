22b2fbc30cd74a328cfa3250dfee7e0de4e82fcb
Kanishk Raj <114609861+KanishkRAJ29@users.noreply.github.com>
2025-07-31 16:58:03 +0530

    Time: 3 ms (84.95%), Space: 12.5 MB (31.6%) - LeetHub

--- 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.cpp
@@ -1,20 +1,37 @@
 class Solution {
 public:
-    int solve(int left,int right,vector<int>&nums,int k){
-        if(left>right)return 0;
+int N;
+    int solve(int left,vector<int>&dp,vector<int>&nums,int k){
+        if(left>N-1)return 0;
+        if(dp[left]!=-1)return dp[left];
         int maxSum=0;
        
         int currMax=-1;
-        for(int i=left;i<=min(right,left+k-1);i++){
+        for(int i=left;i<=min(N-1,left+k-1);i++){
             currMax=max(currMax,nums[i]);
-            int currSum=((i-left+1)*currMax)+solve(i+1,right,nums,k);
+            int currSum=((i-left+1)*currMax)+solve(i+1,dp,nums,k);
 
             maxSum=max(maxSum,currSum);
         }
-        return maxSum;
+        return dp[left]=maxSum;
     }
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n=arr.size();
-        return solve(0,n-1,arr,k);
+        N=n-1;
+        vector<int>dp(n+1,0);
+        for(int left=N;left>=0;left--){
+            int maxSum=0;
+            int currMax=-1;
+            for(int i=left;i<=min(N,left+k-1);i++){
+                
+                currMax=max(currMax,arr[i]);
+                int currSum=((i-left+1)*currMax)+dp[i+1];
+                maxSum=max(maxSum,currSum);
+            }
+            dp[left]=maxSum;
+        }
+        return dp[0];
+       // vector<int>dp(n,-1);
+        //return solve(0,dp,arr,k);
     }
 };
