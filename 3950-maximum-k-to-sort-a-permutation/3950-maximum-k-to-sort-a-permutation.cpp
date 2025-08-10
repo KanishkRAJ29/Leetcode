698000ed133152d912c283b18ca06cefd6c6f467
Kanishk Raj <114609861+KanishkRAJ29@users.noreply.github.com>
2025-08-10 13:55:15 +0530

    Time: 7 ms (39.79%), Space: 160 MB (24.09%) - LeetHub

---
+++ 3950-maximum-k-to-sort-a-permutation/3950-maximum-k-to-sort-a-permutation.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int sortPermutation(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> pos(n);
+
+        for(int i = 0; i < n; i++) pos[nums[i]] = i;
+        
+        int minn = INT_MAX;
+        for (int i= 0; i < n; i++) {
+            if (nums[i] == i) continue;
+            int p = pos[i];
+            minn = minn & nums[i] & nums[p];
+        }
+
+        return minn == INT_MAX ? 0 : minn;
+    }
+};
