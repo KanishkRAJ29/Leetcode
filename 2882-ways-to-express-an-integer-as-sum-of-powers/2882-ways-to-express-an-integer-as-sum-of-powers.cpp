83713f7f83727c1e6c04eb1ab96311c0ce03b094
Kanishk Raj <114609861+KanishkRAJ29@users.noreply.github.com>
2025-08-12 09:56:40 +0530

    Time: 79 ms (75.16%), Space: 73.7 MB (49.02%) - LeetHub

--- 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -1,5 +1,44 @@
 class Solution {
 public:
+        int mod=1e9+7;
+   long long power_with_limit(long long base, int exp, long long limit) {
+        long long res = 1;
+        for (int k = 0; k < exp; ++k) {
+            res *= base;
+            if (res > limit) return res; // early return if > limit
+        }
+        return res;
+    }
+    int numberOfWays(int n, int x) {
+        if (n == 0) return 1;
+
+        // compute max base i such that i^x <= n (integer way)
+        int maxI = 0;
+        for (int i = 1;; ++i) {
+            long long p = power_with_limit(i, x, n);
+            if (p > n) break;
+            maxI = i;
+        }
+        if (maxI == 0) return 0;
+
+        // dp[i][s] = ways to make sum 's' using bases i..maxI
+        vector<vector<int>> dp(maxI + 2, vector<int>(n + 1, 0));
+        dp[maxI + 1][n] = 1; 
+        int N=n;
+        for(int i=maxI;i>0;i--){
+            long long p=power_with_limit(i,x,n);
+            for(int j=n;j>=0;j--){
+                int take=0;
+                if(j+p<=N)take=dp[i+1][j+p];
+                dp[i][j]=(take+dp[i+1][j])%mod;
+            }
+        }
+        return dp[1][0];
+
+    }
+};/*
+class Solution {
+public:
     int mod=1e9+7;
     long long power(long long base, int exp) {
         if(exp==1)return base;
@@ -26,4 +65,4 @@ public:
         vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
         return solve(1,0,x,n,dp);
     }
-};
+};*/
