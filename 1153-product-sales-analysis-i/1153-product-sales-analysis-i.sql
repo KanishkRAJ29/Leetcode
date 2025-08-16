0abd51f698f691ae1c0874ebd1ba42990336bcd0
Kanishk Raj <114609861+KanishkRAJ29@users.noreply.github.com>
2025-08-16 16:33:05 +0530

    Time: 1338 ms (37.7%), Space: 0B (100%) - LeetHub

--- 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
@@ -1,6 +1,8 @@
 # Write your MySQL query statement below
 SELECT
 Product.product_name,Sales.year,Sales.price
-FROM Product right join Sales 
+FROM Product inner join Sales 
 on
 Sales.product_id=Product.product_id
+
+/*i did left join*/
