0f1fa504bb0a026afe1449fbb94656afc0b2340c
Kanishk Raj <114609861+KanishkRAJ29@users.noreply.github.com>
2025-08-27 11:47:27 +0530

    Time: 535 ms (36.36%), Space: 255 MB (23.32%) - LeetHub

--- 2174-next-greater-numerically-balanced-number/2174-next-greater-numerically-balanced-number.cpp
+++ 2174-next-greater-numerically-balanced-number/2174-next-greater-numerically-balanced-number.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     bool solve(int n){
         unordered_map<int,int>mp;
@@ -19,5 +19,34 @@ public:
             i++;
         }
         return -1;
+    }
+};*/
+class Solution {
+
+public:
+    int nextBeautifulNumber(int n) {
+        while(true){
+            n++;
+            vector<int>freq(10,0);
+
+
+            int temp =n;
+            while(temp){
+                freq[temp%10]++;
+                temp/=10;
+            }
+
+
+             bool isBalanced = true;
+             for(int i=0 ; i<10 ; i++){
+                if(freq[i]!=0 && freq[i]!=i){
+                    isBalanced=false;
+                    break;
+                }
+             }
+
+              if (isBalanced) return n; 
+
+        }
     }
 };
