7a268e37677aee2092c18a60b0866526c9f72c64
Kanishk Raj <114609861+KanishkRAJ29@users.noreply.github.com>
2025-09-01 10:14:34 +0530

    Time: 0 ms (100%), Space: 17.2 MB (69.61%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,97 +1,22 @@
 class Solution {
 public:
-
-    vector<vector<int>>dp;
-    int solve(int i, int bought,vector<int>& nums) {
-        if (i >= nums.size()) {
-            return 0;
-        }
-        if(dp[i][bought]!=-1)return dp[i][bought];
-        int sell=0,buy=0;
-        if (bought) {
-
-                // Sell and reset bought
-                 sell=nums[i]+solve(i + 1, 0, nums);
-            
-        } else {
-            // Buy
-            
-              buy=-nums[i]+solve(i + 1, 1, nums);
-        }
-
-        // Skip current day
-        int skip=solve(i + 1, bought, nums);
-        return dp[i][bought]=max({skip,buy,sell});
-    }
-    
     int maxProfit(vector<int>& prices) {
-    int n = prices.size();
-    vector<vector<int>> dp(n, vector<int>(2, 0));
-
-    // Base case:
-    // dp[0][0]: Max profit on day 0 with no stock in hand = 0
-    // dp[0][1]: Max profit on day 0 after buying the stock = -prices[0]
-    dp[0][0] = 0;              
-    dp[0][1] = -prices[0];     
-
-    for (int i = 1; i < n; i++) {
-        for (int j = 0; j <= 1; j++) {
-            int buy = 0, sell = 0;
-
-            if (j == 0) {
-                // If we are in "not holding" state:
-                // Either skip today => dp[i-1][0]
-                // Or sell the stock today => prices[i] + dp[i-1][1]
-                sell = prices[i] + dp[i - 1][1];
-                dp[i][j] = max(dp[i - 1][j], sell);
-            } else {
-                // If we are in "holding" state:
-                // Either skip today => dp[i-1][1]
-                // Or buy stock today => -prices[i] + dp[i-1][0]
-                buy = -prices[i] + dp[i - 1][0];
-                dp[i][j] = max(dp[i - 1][j], buy);
+        int profit=0,i=1,buy=-1;
+        while(i<prices.size()){
+            if(buy==-1){
+                while(i<prices.size() && prices[i-1]>=prices[i]){
+                    i++;
+                }
+                if(i!=prices.size()) buy=prices[i-1];
+            }
+            else{
+                while(i<prices.size() && prices[i-1]<=prices[i]){
+                    i++;
+                }
+                profit+=prices[i-1]-buy;
+                buy=-1;
             }
         }
-    }
-
-    // Return profit at last day in "not holding" state
-    return dp[n - 1][0];
-}
-
-};
-/*class Solution {
-public:
-
-    vector<vector<int>>dp;
-    vector<vector<int>>sellDP;
-    int solve(int i, int bought,vector<int>& nums) {
-        if (i >= nums.size()) {
-            return 0;
-        }
-        if(dp[i][bought]!=-1)return dp[i][bought];
-        int sell=0,buy=0;
-        if (bought) {
-           
-            
-                // Sell and reset bought
-                 sell=nums[i]+solve(i + 1, 0, nums);
-            
-        } else {
-            // Buy
-            
-              buy=-nums[i]+solve(i + 1, 1, nums);
-        }
-
-        // Skip current day
-        int skip=solve(i + 1, bought, nums);
-        return dp[i][bought]=max({skip,buy,sell});
-    }
-
-    int maxProfit(vector<int>& prices) {
-        dp.resize(prices.size()+1,vector<int>(2,-1));
-        
-        return solve(0, 0, prices);
-       
+        return profit;
     }
 };
-*/
