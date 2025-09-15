81bdcc54a962770c21eda54b276cbc69ad16fe85
Kanishk Raj <114609861+KanishkRAJ29@users.noreply.github.com>
2025-09-15 21:49:07 +0530

    Time: 1 ms (33.89%), Space: 9.6 MB (21.59%) - LeetHub

--- 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.cpp
+++ 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.cpp
@@ -1,54 +1,29 @@
 class Solution {
-    vector<string> getAllWords(const string& text) {
-        vector<string> words;
-        string word;
-
-        for(const char& ch : text) {
-            if(isspace(ch)) {
-                words.push_back(word);
-                word = "";
-            }
-            else {
-                word += ch;
-            }
-        }
-        words.push_back(word); // Add last word
-        
-        return words;
-    }
-
-    vector<string> getAllWordsByTokenization(const string& text) {
-        stringstream ss(text);
-        vector<string> words;
-        string word;
-        while(ss >> word) words.push_back(word); // Extract words
-        return words;
-    }
-
-    bool canType(const string& word, const vector<bool>& isPresent) {
-        for(const char& ch : word)
-            if(isPresent[ch - 'a'])
-                return false;
-
-        return true;
-    }
-
 public:
-    // O(N+M+K) & O(N+M) : Where N = total words in "text", M = text.size, K = brokenLetters.size
-    int canBeTypedWords(const string& text, const string& brokenLetters) {
-        vector<string> words = getAllWords(text);
-        
-        vector<bool> isPresent(26);
-        for(const char& ch : brokenLetters) {
-            isPresent[ch - 'a'] = true;
-        }
-
-        int res = 0;
-        for(const auto& word : words) {
-            if(canType(word, isPresent)) {
-                res++;
+    int canBeTypedWords(string t, string b) {
+        vector<string>v;
+        string s="";
+        for(int i=0;i<t.size();i++){
+            if(t[i]==' '){
+                v.push_back(s);
+                s="";
+            }
+            else{
+                s.push_back(t[i]);
             }
         }
-        return res;
+        if(s!="")v.push_back(s);
+        int c=0;
+        for(auto i:v){
+            bool f=false;
+            for(auto j:b){
+                if(i.find(j)!=string::npos){
+                    f=true;
+                    break;
+                }
+            }
+            if(!f)c++;
+        }
+        return c;
     }
 };
