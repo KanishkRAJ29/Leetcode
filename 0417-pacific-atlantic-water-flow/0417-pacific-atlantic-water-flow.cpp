c632358fc6a60dbcf8cfe0bf2ebf4b7b40a3ad18
Kanishk Raj <114609861+KanishkRAJ29@users.noreply.github.com>
2025-10-05 16:42:35 +0530

    Time: 7 ms (62.44%), Space: 23.7 MB (49.17%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -1,59 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
-vector<vector<int>>dir={{1,0},{-1,0},{0,1},{0,-1}};
-int r,c;
-vector<vector<int>>h;
-vector<vector<bool>>bfs(queue<pair<int,int>>&q){
-    vector<vector<bool>>v(r,vector<bool>(c,false));
-    while(!q.empty()){
-        auto cell=q.front();
-        q.pop();
-        int i=cell.first;
-        int j=cell.second;
-        v[i][j]=true;
-        for(int d=0;d<4;d++){
-            int newrow=i+dir[d][0];
-            int newcol=j+dir[d][1];
-            if( newrow<0 || newcol<0 || newrow>=r || newcol>=c){
-                continue;
-            }
-            if( v[newrow][newcol] ){
-                continue;
-            }
-            if(h[newrow][newcol]<h[i][j]){
-                continue;
-            }
-            q.push({newrow,newcol});
+    int m, n;
+
+    void dfs(int r, int c,
+             const vector<vector<int>>& h,
+             vector<vector<int>>& vis) {
+        vis[r][c] = 1;
+        static int dr[4] = {-1, 0, 1, 0};
+        static int dc[4] = {0, -1, 0, 1};
+        for (int k = 0; k < 4; ++k) {
+            int nr = r + dr[k], nc = c + dc[k];
+            if (nr < 0 || nr >= m || nc < 0 || nc >= n) continue;
+            if (vis[nr][nc]) continue;
+            // Reverse flow: can go from (r,c) to (nr,nc) if next height >= current
+            if (h[nr][nc] >= h[r][c]) dfs(nr, nc, h, vis);
         }
     }
-    return v;
-}
+
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        r=heights.size();
-        c=heights[0].size();
-        h=heights;
-        queue<pair<int,int>>pacbfs;
-        queue<pair<int,int>>atlbfs;
-        for(int i=0;i<r;i++){
-            pacbfs.push({i,0});
-            atlbfs.push({i,c-1});
-        }
-        for(int j=1;j<c;j++){
-            pacbfs.push({0,j});
-        }
-        for(int j=0;j<c-1;j++){
-            atlbfs.push({r-1,j});
-        }
-        vector<vector<bool>>pacific=bfs(pacbfs);
-        vector<vector<bool>>atlantic=bfs(atlbfs);
-        vector<vector<int>>ans;
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                if(pacific[i][j] and atlantic[i][j]){
-                    ans.push_back({i,j});
-                }
-            }
-        }
+        m = (int)heights.size();
+        if (m == 0) return {};
+        n = (int)heights[0].size();
+
+        vector<vector<int>> pac(m, vector<int>(n, 0));
+        vector<vector<int>> atl(m, vector<int>(n, 0));
+
+        // Pacific borders: top row & left column
+        for (int c = 0; c < n; ++c) if (!pac[0][c]) dfs(0, c, heights, pac);
+        for (int r = 0; r < m; ++r) if (!pac[r][0]) dfs(r, 0, heights, pac);
+
+        // Atlantic borders: bottom row & right column
+        for (int c = 0; c < n; ++c) if (!atl[m-1][c]) dfs(m-1, c, heights, atl);
+        for (int r = 0; r < m; ++r) if (!atl[r][n-1]) dfs(r, n-1, heights, atl);
+
+        vector<vector<int>> ans;
+        for (int i = 0; i < m; ++i)
+            for (int j = 0; j < n; ++j)
+                if (pac[i][j] && atl[i][j]) ans.push_back({i, j});
         return ans;
     }
 };
