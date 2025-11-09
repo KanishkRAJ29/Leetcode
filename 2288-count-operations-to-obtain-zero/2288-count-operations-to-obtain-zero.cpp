6633108db71e7d633f03fb2900932c10d1943a64
Kanishk Raj <114609861+KanishkRAJ29@users.noreply.github.com>
2025-11-09 11:20:26 +0530

    Time: 0 ms (100%), Space: 7.9 MB (40.22%) - LeetHub

--- 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.cpp
+++ 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     int ans=0;
     void count(int a,int b){
@@ -20,5 +20,18 @@ public:
     int countOperations(int num1, int num2) {
         count(num1,num2);
         return ans;
+    }
+};*/
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        int res = 0;  // total number of subtraction operations
+        while (num1 && num2) {
+            // each step of the Euclidean algorithm
+            res += num1 / num2;
+            num1 %= num2;
+            swap(num1, num2);
+        }
+        return res;
     }
 };
